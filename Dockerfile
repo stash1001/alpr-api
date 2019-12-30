735d7bb0512ae079f3a3febd7444872dc9567bbf
Martin Hempstock <martin.hempstock@anaplan.com>
2019-12-30 08:43:03 +0000

    added livelyness and readyness endpoint

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM stash1001/openalpr
 RUN  apt-get update && apt-get install -y python-pip && apt-get clean
 RUN pip install Flask
 COPY app.py app.py 
+EXPOSE 5000/tcp
 
 entrypoint []
 CMD python app.py
--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, make_response
 app = Flask(__name__)
 
 
+@app.route('/health', methods=['GET'])
+def health():
+    return make_response("healthy", 200)
+
+
 @app.route('/', methods=['POST'])
 def simple():
     print("Got it 2")
-    return make_response("", 200)
+    return make_response("healthy", 200)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
